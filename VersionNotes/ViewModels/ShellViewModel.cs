4eb1e5b2ba16cea67ef8160deadde4a9ba6a6841
Robertson Brinker <robertsonbrinker@gmail.com>
2023-04-04 14:52:50 -0400

    setting download link to current release folder

--- VersionNotes/ViewModels/ShellViewModel.cs
+++ VersionNotes/ViewModels/ShellViewModel.cs
@@ -130,7 +130,7 @@ namespace VersionNotes.ViewModels
             }
         }
 
-        private string _downLoadLink = "https://drive.google.com/drive/folders/1x4-4vjZS4AM6HNehp6XX66TZP68Q68f5";
+        private string _downLoadLink = "https://drive.google.com/drive/folders/1dCpaXhp3OWmNMDNRi-a8fWgqon17Mm22";
         public string DownLoadLink
         {
             get => _downLoadLink;
